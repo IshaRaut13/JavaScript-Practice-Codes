e93d47912930134e1c8c6a574e9ea44deadba7d6
IshaRaut13 <139954745+IshaRaut13@users.noreply.github.com>
2025-12-16 20:21:30 +0530

    Number is divisible by 5 or not using if-else statement

---
+++ practice3.js
@@ -0,0 +1,8 @@
+let divisible = prompt("Enter the number")
+console.log("Enter the number:", divisible)
+
+if (divisible%5 === 0){
+    console.log(divisible, "is divisible by 5")
+} else {
+    console.log(divisible, "is not divisible by 5")  
+}
