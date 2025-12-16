2905a5c69ce1cb3867f6f2f47aee8329c5f56eb8
IshaRaut13 <139954745+IshaRaut13@users.noreply.github.com>
2025-12-16 19:02:30 +0530

    practice example code 1

---
+++ practice1.js
@@ -0,0 +1,8 @@
+const product = {
+    product_name : 'Parker Ball Pen',
+    rating : 4,
+    offer : 5,
+    price : 270,
+};
+
+console.log(product);
---
+++ practice2.js
@@ -0,0 +1,9 @@
+const profile = {
+    profileName: 'Isha Raut',
+    follower: 564,
+    following: 4,
+    posts: 195,
+    isFollow: false,
+}
+
+console.log(profile)
