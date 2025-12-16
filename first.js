ce144795b222f7ac955d729fd82b254a520f536e
IshaRaut13 <139954745+IshaRaut13@users.noreply.github.com>
2025-12-16 18:48:37 +0530

    first js code

---
+++ first.js
@@ -0,0 +1,23 @@
+// variables can have $, _, letter, digits, nooooo space
+fullname = 'isha raut';
+fullname123 = 'isha raut';
+full_name = 'isha raut';
+fullname$ = 'isha raut';
+
+// variables can start will letter, underscore, dollar
+fullname = 'isha raut';
+_fullname = 'isha raut';
+$fullname = 'isha raut';
+
+// variable names are case sensitive
+// reserved names can't be variable names
+
+// how to define variables 
+var fullname='isha'    
+let full_name='isha'
+const fullname='isha'
+
+console.log("Isha Raut");
+console.log("hello");
+
+// if const is an object than its key's value can be changed
